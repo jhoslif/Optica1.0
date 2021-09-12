d1ea7ec441de93c05fa4b0abc6d25e85294e7dd6
joselyntrix <77370304+joselyntrix@users.noreply.github.com>
2021-09-12 15:09:32 -0500

    Etructura Html

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -1,3 +1,51 @@
 <x-guest-layout>
-    
+    <!--header-->
+    <div >
+        <div><!--LEFT-->
+            <img src="" alt="">
+            <a href="#">INICIO</a>
+            <a href="#">CATALOGO EN LINEA</a>
+            <a href="#">AGENDAR CITA</a>
+            <a href="#">CONTACTOS</a>
+
+        </div><!--right-->
+        <div >
+            <a href="#">Login</a>
+            <a href="#">Register</a>
+        </div>
+    </div>
+    <!--hero section-->
+    <div>
+        <div><!--left-->
+        <h2>Welcome to my website</h2>
+        <p>La vida es fascinante,sólo hay que mirarla a traves de las gafas correctas</p>
+        <a href="#">Acerca de Nostros</a>
+
+        </div>
+        <div> <!--rigth-->
+        <img src="" alt="">
+        </div>
+    </div>
+    <div>
+     <div class="">
+         <img src="" alt="">
+             <p>Lentes Oftálmicos</p>
+         </img>
+     </div>
+     <div class="">
+         <img src="" alt="">
+             <p>Lentes de Protección Azul</p>
+         </img>
+     </div>
+     <div class="">
+         <img src="" alt="">
+             <p>Gafas de Sol</p>
+         </img>
+     </div>
+     <div class="">
+         <img src="" alt="">
+             <p>Lentes Oftálmicos y Armazones</p>
+         </img>
+     </div>
+    </div>
 </x-guest-layout>

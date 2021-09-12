10bc713b2579ab584e69a81f5c820f14086faf22
joselyntrix <77370304+joselyntrix@users.noreply.github.com>
2021-09-12 14:30:53 -0500

    laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/landing-page', function () {
+    return view('landing-page');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
